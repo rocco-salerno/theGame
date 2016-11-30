e3c32f8755906a87aeea4b0532f8c03d006c816e
Vincent Torba <vincenttorba@yahoo.com>
2016-11-30 15:35:12 -0500

    Update User.h
    We need <cstdlib> so we can get a random int

--- User.h
+++ User.h
@@ -1,16 +1,17 @@
 //header file for User
 #include <iostream>
 #include <string>
+#include <cstdlib> //allows the use of rand to get a random integer used by rand() % num (this would do 0 to num-1)
 using namespace std;
-
+i
 class User
 {
 public:
 	string SetName();//names characters
-    int Attack();//how the attack works
-	HP();//increment/decrement hp
+   	int Attack();//how the attack works
+	int HP();//increment/decrement hp
 private:
-	int m_hp;
+	int m_hp=40;
 
 };
 #endif
