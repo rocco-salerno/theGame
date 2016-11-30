7bbc1dc345fbc0faef71926080255281e106fa5b
Vincent Torba <vincenttorba@yahoo.com>
2016-11-30 15:11:31 -0500

    Update User.h

--- User.h
+++ User.h
@@ -1,11 +1,18 @@
 //header file for User
 #include <iostream>
+#include <string>
 using namespace std;
 
 class User
 {
+public:
+	string SetName();//names characters
+    int Attack();//how the attack works
+	HP();//increment/decrement hp
+private:
+	int m_hp;
 
+};
+#endif
 
-
-}
 
