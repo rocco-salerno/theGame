967588b5ab5cfd30ef1dacb7f4a3ef9aacd8d560
nick9797 <nicks9795@gmail.com>
2016-12-02 10:59:35 -0500

    Update User.h

--- User.h
+++ User.h
@@ -3,15 +3,18 @@
 #include <string>
 #include <cstdlib> //allows the use of rand to get a random integer used by rand() % num (this would do 0 to num-1)
 using namespace std;
-i
+
 class User
 {
 public:
 	string SetName();//names characters
    	int Attack();//how the attack works
 	int HP();//increment/decrement hp
+	int health(); //health the user has
+	int damage(); //damage the user inflicts
 private:
 	int m_hp=40;
+	int damage=rand() %6+5
 
 };
 #endif
