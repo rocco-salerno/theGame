cf4f5f2ba0d832f2a0c8bab17a00f3b2b1da505d
nick9797 <nicks9795@gmail.com>
2016-12-02 11:41:45 -0500

    Update gameplay.cpp

--- gameplay.cpp
+++ gameplay.cpp
@@ -24,7 +24,49 @@ else
 	return 0;
 }
 
+
+int combat()
+{
+int enemy[4];
+int potion=3;
+int damage=2;
+int enemyhp=10;
+	cout<<"An"<<enemy<<"appears!"<<endl;
+	cout<<"What will you do?"<<endl;
+int battle=2;
+while(battle==2){
+	cout<<"Press 1 to attack, or 2 to use a potion."<<endl;
+	cin>>battle;
+if(battle==1)
+	cout<<"You used your sword!"<<endl;
+	cout<<damage<<"points of damage to"<<enemy<<endl;
+	enemyhp=enemyhp-10;
+if(battle==2)
+	cout<<"You used a potion!"<<endl;
+	cout<<"You healed ten HP!"<<endl;
+	potion=potion-1;
+	cout<<"You have:"<<potion<<"potions left."<<endl;
+if(enemyhp<=0)
+	cout<<"You have defeated the enemy!"<<endl;
+	battle=1;
+	break;
+}
+}
+
+int ending()
+{
+cout<<"You have completed the game! Congratulations!"<<endl;
+}
+
+int gameover()
+{
+cout<<"You have fallen. Game over!"<<endl;
+}
+
 int main()
 {
 startgame();
+combat();
+ending();
+gameover();
 }
