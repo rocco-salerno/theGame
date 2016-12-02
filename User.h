57b55efe53ccd9fd96c8117f989e5a28ac9cea92
Carnage08 <rocco.m.salerno@gmail.com>
2016-12-02 13:51:17 -0500

    Update User.h

--- User.h
+++ User.h
@@ -1,4 +1,6 @@
-//header file for User
+#ifndef USER_H_
+#define USER_H_
+
 #include <iostream>
 #include <string>
 #include <cstdlib> //allows the use of rand to get a random integer used by rand() % num (this would do 0 to num-1)
@@ -7,16 +9,16 @@ using namespace std;
 class User
 {
 public:
-	string SetName();//names characters
-   	int Attack();//how the attack works
-	int HP();//increment/decrement hp
+	string setName();//names characters
+   	int attack();//how the attack works
+	int increaseHP();//increment/decrement hp
 	int health(); //health the user has
-	int damage(); //damage the user inflicts
 private:
 	int m_hp=100;
-	int damage=rand() %6+5
+	int damage=rand() %6+5;
 
 };
-#endif
 
 
+
+#endif /* USER_H_ */
