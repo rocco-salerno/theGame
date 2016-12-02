18e319ee1e59726af25306d57117b3db0e39509f
nick9797 <nicks9795@gmail.com>
2016-12-02 11:13:14 -0500

    Add files via upload

---
+++ gameplay.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int startgame()
+{
+int choice;
+string name;
+cout<<"Would you like to play a game? Press the one button to start, otherwise hit anything else to end."<<endl;
+cin>>choice;
+if(choice==1)
+{
+	cout<<"Welcome!"<<endl;
+	cout<<"Enter your name: "<<endl;
+	cin>>name;
+	cout<<"Hello there,"<<name<<"!"<<endl;
+	cout<<"Welcome to the land of dungeons and dragons! Your goal is to defeat the dragon that attacked our village!"<<endl;
+	cout<<"You will be given a sword and some potions. Best of luck!"<<endl;
+}
+else
+{
+	cout<<"Ending program"<<endl;
+}
+}
+
+int main()
+{
+startgame();
+}
