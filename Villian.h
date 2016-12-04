68936802836ca3c02788c66cc5b8d1fefd60b953
nick9797 <nicks9795@gmail.com>
2016-12-04 10:17:38 -0500

    Update Villian.h

--- Villian.h
+++ Villian.h
@@ -1,13 +1,25 @@
 //header for all Villians
+#include <iostream>
+using namespace std;
+#include "user.h"
+
 
 class Villians
 {
-    class Dragon
+    //class Dragon
     {
+        public:
+            string name;
+            int hp;
+            int damage;
+        
+            Villains(string enemyName, int current_HP, int attack_damage);
+        
+            void attack(user&);
     
     }
-    class Skeletons
-    {
+    //class Skeletons
+    //{
     
-    }
+    //}
 }
