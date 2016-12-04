2343d448583059b49757aad66da58470bcbd974c
nick9797 <nicks9795@gmail.com>
2016-12-04 10:27:53 -0500

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -2,12 +2,22 @@
 #include <cstdlib>
 using namespace std;
 
-int User::attack()
+void User::attack(Villains& enemy)
 {
-	int damage = rand()%6 +5;
-    return damage;
+	int damage = rand()%6 +5;	
+	enemy.health-=damage;
+	cout<<"You used your sowrd! You inflicted: "<<damage<<"to the enemy!"<<endl;
+   	return damage;
 }
 
+void User::potions(User)
+	if (potions==0)
+		cout<<"You are out of potions!"<<endl;
+	else
+		cout<<"You used a potion!"<<endl;
+		cout<<"You have gained ten HP back!"<<endl;
+		currenthealth=currenthealth+10;
+	return potions;
 
 int User::health()
 {
