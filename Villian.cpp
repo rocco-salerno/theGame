07bab5768ca567882352d39339443f9356c5eaf8
nick9797 <nicks9795@gmail.com>
2016-12-04 10:37:57 -0500

    Update Villian.cpp

--- Villian.cpp
+++ Villian.cpp
@@ -1,1 +1,24 @@
 //Villian.cpp
+#include "villain.h"
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void Villains::attack(User& enemy)
+{
+   int damage= rand%4 +3;
+   enemy.health-=damage;
+   cout<<"You are attacked by the"<<Villains<<"!"<<endl;
+   cout<<"You have lost"<<damage<<"HP."<<endl;
+}
+
+Villains::Villains(string enemyname, int enemyhealth, int enemydamage)
+{
+  name=enemyname;
+  health=enemyhealth;
+  damage=enemydamage;
+}
+     
+     
