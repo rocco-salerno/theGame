4e113a1149faf3321ab2da1ff2b42e1a0e0ae432
nick9797 <nicks9795@gmail.com>
2016-12-07 12:54:48 -0500

    Update Villian.h

--- Villian.h
+++ Villian.h
@@ -8,17 +8,18 @@ class Villians
     {
         public:
             string enemyname=Skeleton;
-            int enemyhealth=10;
-            int enemydamage=2;
+            int m_enemyhealth;
+            int m_enemydamage;
         
             void attack(User& enemy);
     
     };
     class Wolf:public Villian
     {
+        public:
         string enemyname=Wolf;
-        int enemyhealth=15;
-        int enemydamage=4;
+        int m_enemyhealth=15;
+        int m_enemydamage=4;
         
         void attack(User& enemy);
         
@@ -26,19 +27,24 @@ class Villians
     
     class Centaur:public Villian
     {
+        public:
         string enemyname=Centaur;
-        int emenyhealth=25;
-        int enemydamage=6;
+        int m_enemyhealth=25;
+        int m_enemydamage=6;
         
         void attack(User& enemy);
     };
    
     class Dragon:public Villian
     {
+        public:
         string enemyname=Dragon;
-        int enemyhealth=40;
-        int enemydamage=10;
+        int m_enemyhealth=40;
+        int m_enemydamage=10;
         
         void attack(User& enemy);
     };
+    private:
+        int m_enemyhealth;
+        int m_enemydamage;
 };
