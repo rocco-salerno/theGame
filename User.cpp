7302c6ec36571812a38bfc997444372197ad56b9
Vincent Torba <vincenttorba@yahoo.com>
2016-12-07 16:14:22 -0500

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -1,38 +1,38 @@
-#include "User.h"
+#include <iostream>
 #include <cstdlib>
-#include <ctime>
+#include "User.h"
+#include "Villain.h"
 using namespace std;
 
-//class for user to attack the enemy
-int User::attack(Villains& enemy)
+int User::attack(class Villain&)
 {
 	srand(time(NULL));
-	int damage = rand()%6 +5;//random attack number generator	
-	enemy.enemyhealth-=damage; // enemy health is subtracted from damage generated
-	cout<<"You used your sowrd! You inflicted: "<<damage<<"to the enemy!"<<endl;
+	int damage = rand()%6 +5;//random attack number generator
+	Villain.getHealth()-=damage; // enemy health is subtracted from damage generated
+	cout<<"You used your sword! You inflicted: "<<damage<<"to the enemy!"<<endl;
    	return damage;
 }
 
-int User::potions(User) //class for user to use potions
+void User::setHealth()
 {
-	if (potions==0) // if user has no more potions,
+	m_hp =100;
+}
+void User::setPotions()
+{
+	m_potions =5;
+}
+void User::potions(class User) //class for user to use it
+{
+	getHealth();
+	int potions= m_potions;
+	if (potions==0) // if user has no more it
 		cout<<"You are out of potions!"<<endl; //print this.
 	//if the use has potions, use one and add 10 hp to user health
 	else
 	{
 		cout<<"You used a potion!"<<endl;
 		cout<<"You have gained ten HP back!"<<endl;
-		potions=potions-1;
-		currenthealth=currenthealth+10;
-		return potions;
+		m_health= m_health + 10;
 	}
-}
 
-
-
-User::User() //(string username)
-{
-	//string name=username;//how would the string name get called if we have it in gameplay?
-	int m_hp; //max health for user
-	int potions=3; //user starts with 3 potions
 }
