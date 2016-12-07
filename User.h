ad001379cb6bb66b1c09b6f244f25f1751d1838b
Vincent Torba <vincenttorba@yahoo.com>
2016-12-07 15:57:38 -0500

    Update User.h

--- User.h
+++ User.h
@@ -1,3 +1,4 @@
+
 #ifndef USER_H_
 #define USER_H_
 
@@ -10,17 +11,19 @@ using namespace std;
 class User
 {
 public:
-	string setName();//names characters
-   	//int attack();//how the attack works
-	int increaseHP();//increment/decrement hp
-	int health(); //health the user has
-	void attack(Villains& enemy); //Has user attack villain
+	void potions(class User);//health the user has
+	int attack(class Villain&); //Has user attack villain
+	int getHealth(){return m_hp;}
+	void setHealth();
+	void setPotions();
 private:
-	int m_hp=100;
-	int potions= 5; //5 potions for the user.
+	int m_hp;//100
+	int m_potions; //5 potions for the user.
 
 };
 
 
 
+
 #endif /* USER_H_ */
+
