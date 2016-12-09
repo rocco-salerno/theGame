1f3ec289787b4c7d2a885228d01c8fceed1d3518
Vincent Torba <vincenttorba@yahoo.com>
2016-12-09 14:27:33 -0500

    Update User.h

--- User.h
+++ User.h
@@ -1,23 +1,22 @@
-
 #ifndef USER_H_
 #define USER_H_
 
-#include "Villain.h"
 #include <iostream>
 #include <string>
 #include <cstdlib> //allows the use of rand to get a random integer used by rand() % num (this would do 0 to num-1)
 using namespace std;
+class Villains;
 
 class User
 {
 public:
-	void potions(class User);//health the user has
-	int attack(class Villain&); //Has user attack villain
+	void potions(User&);//health the user has
+	int attack(Villains& enemy);
 	int getHealth(){return m_hp;}
 	void setHealth();
 	void setPotions();
+	int m_hp;
 private:
-	int m_hp;//100
 	int m_potions; //5 potions for the user.
 
 };
@@ -26,4 +25,6 @@ private:
 
 
 #endif /* USER_H_ */
+
+
 
