21b41e99ff41520f3c9e49f0255eb5cfcddc7c97
Vincent Torba <vincenttorba@yahoo.com>
2016-12-09 14:28:17 -0500

    Update Villian.h

--- Villian.h
+++ Villian.h
@@ -1,61 +1,64 @@
 #ifndef VILLAINS_H_
 #define VILLAINS_H_
+class User;
 
-
-
-class Villains{
+class Villains
+{
 public:
-	    class Skeleton:public Villains
-	    {
-	    public:
-	        // int m_enemyhealth=10; 
-	        //int m_enemydamage=3;
-	    	void attack(User&);
-	    	void setHealth();
-	    	int getHealth(){return m_enemyhealth;}
-	    	void setDamage();
+	virtual int attack(User& user);
+	virtual ~Villains();
+	int m_enemyHealth;
 
 
-	    };
-	    class Wolf:public Villains
-	    {
-	     public:
-	        //int m_enemyhealth=15;
-	        //int m_enemydamage=4;
-	         void attack(User&);
-	         void setHealth();
-	         int getHealth(){return m_enemyhealth;}
-	         void setDamage();
-
-	    };
-
-	    class Centaur:public Villains
-	    {
-	     public:
-	        //int m_enemyhealth=25;
-	        //int m_enemydamage=6;
-	        void attack(User&);
-	        void setHealth();
-	        int getHealth(){return m_enemyhealth;}
-	        void setDamage();
-	    };
-
-	    class Dragon:public Villains
-	    {
-	     public:
-	       //int m_enemyhealth=40;
-	       // int m_enemydamage=10;
-	        void attack(User&);
-	        void setHealth();
-	        int getHealth(){return m_enemyhealth;}
-	        void setDamage();
-	    };
-	private:
-	    int m_enemyhealth;
-	    int m_enemydamage;
 
 };
 
+class Skeleton : public Villains
+{
+public:
+		// int m_enemyhealth=10;
+	    //int m_enemydamage=3;
+	   int attack(User& user);
+	    void setHealth();
+	    int getHealth(){return m_enemyHealth;}
+	    void setDamage();
+};
+
+class Wolf: public Villains
+{
+public:
+	 //int m_enemyhealth=15;
+	 //int m_enemydamage=4;
+	 int attack(User& user);
+	 void setHealth();
+	 int getHealth(){return m_enemyHealth;}
+	 void setDamage();
+
+};
+
+class Centaur:public Villains
+{
+public:
+	    	//int m_enemyhealth=25;
+	        //int m_enemydamage=6;
+	        int attack(User& user);
+	        void setHealth();
+	        int getHealth(){return m_enemyHealth;}
+	        void setDamage();
+	    };
 
 
+class Dragon:public Villains
+{
+public:
+		 //int m_enemyhealth=40;
+		 // int m_enemydamage=10;
+		 int attack(User& user);
+		 void setHealth();
+		 int getHealth(){return m_enemyHealth;}
+		 void setDamage();
+
+};
+
+#endif /* VILLAINS_H_ */
 #endif /* VILLAINS_H_ */
