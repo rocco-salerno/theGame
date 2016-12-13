7e86f0119a69d6786b85fb07aa810929a9836f0b
nick9797 <nicks9795@gmail.com>
2016-12-13 14:54:41 -0500

    Add files via upload

--- gameplay.cpp
+++ gameplay.cpp
@@ -6,28 +6,7 @@
 #include "Villian.h"
 using namespace std;
 
-int startgame()
-{
-int choice;
-string name;
-cout<<"Would you like to play a game? Press the 1 button to start, otherwise hit anything else to end."<<endl;
-cin>>choice;
-if(choice==1)
-{
-	cout<<"Welcome!"<<endl;
-	cout<<"Enter your name: "<<endl;
-	cin>>name;
-	cout<<"Hello there,"<<name<<"!"<<endl;
-	cout<<"Welcome to the land of dungeons and dragons! Your goal is to defeat the dragon that attacked our village!"<<endl;
-	cout<<"Just take note that there will be other enemies in your way!"<<endl;
-	cout<<"You will be given a sword and some potions. Best of luck!"<<endl;
-}
-else
-{
-	cout<<"Ending program"<<endl;
-}
-	return 0;
-}
+
 
 int gameover()
 {
@@ -109,8 +88,31 @@ while(enemy[i]->m_enemyHealth > 0 && name.m_hp >0 && close<=3)
 }
 }
 
+int startgame()
+{
+int choice;
+string name;
+cout<<"Would you like to play a game? Press the 1 button to start, otherwise hit anything else to end."<<endl;
+cin>>choice;
+if(choice==1)
+{
+	cout<<"Welcome!"<<endl;
+	cout<<"Enter your name: "<<endl;
+	cin>>name;
+	cout<<"Hello there,"<<name<<"!"<<endl;
+	cout<<"Welcome to the land of dungeons and dragons! Your goal is to defeat the dragon that attacked our village!"<<endl;
+	cout<<"Just take note that there will be other enemies in your way!"<<endl;
+	cout<<"You will be given a sword and some potions. Best of luck!"<<endl;
+	combat();
+}
+else
+{
+	cout<<"Ending program"<<endl;
+}
+	return 0;
+}
+
 int main()
 {
 	startgame();
-	combat();
 }
